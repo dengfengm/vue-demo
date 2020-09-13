260783766ba19894148f53fa3cf5596fbf8c8715
mzj <825241378@qq.com>
2020-09-13 21:45:42 +0800

    demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    {{ msg }}
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -11,6 +12,12 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+  data () {
+    return {
+      url: '',
+      msg: 'hello mzj!'
+    }
+  },
   components: {
     HelloWorld
   }
---
+++ src/views/Map.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="about">
+    <h1>This is an demo page</h1>
+    <div>
+      <input type="text" placeholder="标题" v-model="obj.title" />
+      <input type="text" placeholder="发布人" v-model="obj.user" />
+      <input type="date" placeholder="发布时间" v-model="obj.date" />
+      <button @click="add()">新增</button>
+    </div>
+    <div>
+      <table cellpadding="5" cellspacing="0">
+        <thead>
+          <tr>
+            <th>序号</th>
+            <th>标题</th>
+            <th>发布人</th>
+            <th>发布时间</th>
+            <th>操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(v,i) in lists" :key="i">
+            <td>{{i+1}}</td>
+            <td>{{v.title}}</td>
+            <td>{{v.user}}</td>
+            <td>{{v.date}}</td>
+            <td>
+              <button @click="del(i)">删除</button>
+              <button @click="edit(i)">编辑</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div id="layer" v-show="flag">
+        <div>
+          编辑
+          <span @click="flag=false">x</span>
+        </div>
+        <div>
+          <input type="text" placeholder="标题" v-model="editobj.title"/>
+          <input type="text" placeholder="发布人"  v-model="editobj.user"/>
+          <input type="date" placeholder="发布时间"  v-model="editobj.date"/>
+          <button @click="update()">更新</button>
+          <button @click="flag=false">取消</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Map',
+  data () {
+    return {
+      obj: {
+        title: '',
+        user: '',
+        date: ''
+      },
+      editobj: {
+        title: '',
+        user: '',
+        date: ''
+      },
+      lists: [
+        { title: '标题1', user: 'a', date: '2020-02-01' },
+        { title: '标题2', user: 'b', date: '2020-02-03' }
+      ],
+      flag: false,
+      index: 0
+    }
+  },
+  methods: {
+    add: function () {
+      const { title, user, date } = this.obj
+      if (!title || !user || !date) return
+      this.lists.push({ title, user, date })
+      this.obj = {}
+    },
+    del: function (i) {
+      this.lists.splice(i, 1)
+    },
+    edit: function (i) {
+      this.index = i
+      this.flag = true
+      this.editobj = { ...this.lists[i] }
+    },
+    update: function () {
+      this.lists[this.index] = this.editobj
+      this.flag = false
+    }
+  }
+}
+</script>
